b23f5b77743b79312b5291d393055fff3b786ccc
Jeremy S <Jeremy@jsmoog.com>
2025-01-14 14:43:47 +0000

    cargo update

--- termreader-sources/src/lib.rs
+++ termreader-sources/src/lib.rs
@@ -4,9 +4,11 @@ pub mod sources;
 
 use anyhow::Result;
 use chapter::{Chapter, ChapterPreview};
+use std::fmt::Debug;
 
-pub fn get_html<T: reqwest::IntoUrl>(url: T) -> Result<String> {
+pub fn get_html<T: reqwest::IntoUrl + Debug>(url: T) -> Result<String> {
     let c = reqwest::blocking::ClientBuilder::new()
+        .use_rustls_tls()
         .user_agent("Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0");
     let client = c.build()?;
     let text = client.get(url).send()?.text()?;
@@ -15,6 +17,7 @@ pub fn get_html<T: reqwest::IntoUrl>(url: T) -> Result<String> {
         || text.contains("Checking if the site connection is secure")
         || text.contains("Verify below to continue reading")
     {
+        eprintln!("ERROR:\n\n\n\n{}\n\n\n\n", text);
         panic!("Cloudflare reached, error handling currently unimplemented.");
     } else {
         Ok(text)
